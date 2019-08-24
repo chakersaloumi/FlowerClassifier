f20a28dd627356f2f672da8ffa976558f45e8bdb
Chaker Saloumi <chaker.saloumi@gmail.com>
2019-08-24 18:38:47 +0200

    Adding Image Picker functionality

--- FlowerClassifier/ViewController.swift
+++ FlowerClassifier/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    @IBOutlet var imageDisplayed: UIImageView!
+    //imageTaken
+    let imageTaker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Setting as delegate
+        imageTaker.delegate = self
+        //using camera
+        imageTaker.sourceType = .camera
+        //No editing
+        imageTaker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageDisplayed.image = userImage
+        }
+        
+        imageTaker.dismiss(animated: true, completion: nil)
+        
     }
 
 
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        present(imageTaker, animated: true, completion: nil)
+    }
+    
 }
 
