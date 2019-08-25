bb927fccf6fe895e4f1ccca2bf6e7413200b0436
Chaker Saloumi <chaker.saloumi@gmail.com>
2019-08-25 10:52:23 +0200

    Model loading and requests are added

--- FlowerClassifier/ViewController.swift
+++ FlowerClassifier/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 import Vision
+import CoreML
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     @IBOutlet var imageDisplayed: UIImageView!
     //imageTaken
     let imageTaker = UIImagePickerController()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -28,15 +29,54 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         if let userImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
             imageDisplayed.image = userImage
+            //convert userImage
+            guard let ciimage = CIImage(image: userImage) else {
+                fatalError("Error converting Image ")
+            }
+            
+            //feed the image to the detection function
+            detectFlower(flowerImage: ciimage)
+            
         }
         
         imageTaker.dismiss(animated: true, completion: nil)
         
     }
-
-
+    
+    func detectFlower(flowerImage image: CIImage){
+        
+        //load model
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Loading model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) {(request, error) in
+            guard let results =  request.results as? [VNClassificationObservation] else { // classification analysis processed by the request
+                fatalError("Processing model failed ")
+            }
+            
+            if let mostProbableFlower = results.first {
+                self.navigationItem.title = mostProbableFlower.identifier.capitalized
+                
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            
+            try! handler.perform([request])
+            
+        } catch {
+            
+            print(error)
+        }
+        
+    }
+    
+    
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
         present(imageTaker, animated: true, completion: nil)
     }
